3c6035d413194e336e7866e284d8bd1c60a91e40
roman volodin <roman.volodin@gmail.com>
2018-06-27 20:44:23 +0300

    add basic functionality

--- bars.py
+++ bars.py
@@ -1,21 +1,42 @@
 import json
+import math
 
 
 def load_data(filepath):
-    pass
+    with open(filepath, 'r', encoding='utf-8') as json_file:
+        return json.load(json_file)
 
 
 def get_biggest_bar(data):
-    pass
+    return max(
+        data['features'],
+        key=lambda item: item['properties']['Attributes']['SeatsCount']
+    )
 
 
 def get_smallest_bar(data):
-    pass
+    return min(
+        [
+            item for item in data['features']
+            if int(item['properties']['Attributes']['SeatsCount']) > 0
+        ],
+        key=lambda item: item['properties']['Attributes']['SeatsCount']
+    )
 
 
 def get_closest_bar(data, longitude, latitude):
-    pass
+    return min(
+        data['features'],
+        key=lambda item: math.sqrt(
+            (item['geometry']['coordinates'][1] - longitude)**2 + (item['geometry']['coordinates'][0] - latitude)**2
+        )
+    )
 
 
 if __name__ == '__main__':
-    pass
+    path = "bars.json"
+    data = load_data(path)
+    biggest = get_biggest_bar(data)['properties']['Attributes']
+    smallest = get_smallest_bar(data)['properties']['Attributes']
+    closest = get_closest_bar(data, 55.667587, 37.556106)
+    print(closest)
